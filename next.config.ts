f797a3b29f7e8c95aa916e602fd9443d655e657d
Akmal Raafid Taufiqurrahman <akmalraafid25@gmail.com>
2025-11-01 00:20:58 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+    env: {
+      SNOWFLAKE_TOKEN: process.env.SNOWFLAKE_TOKEN,
+    },
+    eslint: {
+      // ✅ This skips all ESLint checks during `next build`
+      ignoreDuringBuilds: true,
+    }
 };
 
 export default nextConfig;
