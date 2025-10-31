7d06e5c172d8fa7c811bd2d9b4dd8098f45d273c
Akmal Raafid Taufiqurrahman <akmalraafid25@gmail.com>
2025-10-31 22:33:50 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['snowflake-sdk']
-  },
-  webpack: (config) => {
-    config.externals.push({
-      'snowflake-sdk': 'commonjs snowflake-sdk'
-    })
-    return config
-  }
+
 };
 
 export default nextConfig;
